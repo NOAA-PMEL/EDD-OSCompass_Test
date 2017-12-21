425f12de587c12f6d48a19c7f08c1d6143d34f19
AlexMTurpin <alex.turpin@noaa.gov>
2017-12-21 14:32:04 -0800

    Update calibration.py

--- Software/calibration.py
+++ Software/calibration.py
@@ -1,58 +1,112 @@
+import errno
 import csv
 import time
+import os
 
 import device
 import params
 
-def calibrate(port):
+
+def parameter_dump(compass, filename):
     '''
     Run automated parameter setting and calibration for the device at the specified port
     '''
-    # configure the serial connections (the parameters differs on the device you are connecting to)
-    with device.compass_conn(port) as ser:
-        print("Port open = {}".format(ser.isOpen()))
+    
+    # dump params into log
+    paramdump = device.command(compass.ser,'paramdump')
+    compass.serialNumber = (paramdump[paramdump.find("Serial_number=")+14:paramdump.find("Test_date")]).strip("\r\n")
+    
+    
+    #Create Compass Serial Number directory if non-existent. 
+    try:
+        os.mkdir('%s/%s/' % (os.getcwd(), compass.serialNumber))
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise  
+        pass
+    
+    #Save parameters into serial number directory
+    paramfname = '%s/%s/%s.txt' % (os.getcwd(), compass.serialNumber, filename)
+    with open(paramfname, 'w') as paramfile:
+        paramfile.write(paramdump)
+    paramfile.close()
+    
+    
+def parameter_check(compass, filename):    
 
-        #confirm data can be read
-        data = ser.read_until().decode()
-        print("Data string = {}".format(data))
+    paramfname = '%s/%s/%s.txt' % (os.getcwd(), compass.serialNumber, filename)
+    #load/parse params and print out
+    with open(paramfname, 'r') as paramfile:
+        paramdict = params.paramdump_to_dict(paramfile.read())
+    print("Saving parameters to %s.txt in directory: /%s/" % (filename, compass.serialNumber) )
+    #for key,value in paramdict.items():
+        #print("{0} = {1}".format(key,value))
+    
+    #load conditions are determine params outside of valid range
+    conditions = params.param_conditions('./data/param_conditions.csv')
+    bad_params = params.invalid_params(paramdict,conditions)
+    if(bad_params):
+        print("Bad Params: %s" % bad_params)
+    
+    #update settable params
+    settable_params = [param for param in bad_params if param in device.set_commands.keys()]
+    if settable_params:
+        print("Setting parameters:")
+        for param in settable_params:
+            current = paramdict[param]
+            target = int(params.valid_param(conditions[param]))
+            #print("Target: %d" % target)
+            device.command(compass.ser, param, target)
+            print("{param} : {current} --> {target} Done".format(param=param,
+                                                                 current = current,
+                                                                 target = target))
+    else:
+        print("No settable invalid parameters")
 
-        # dump params into log
-        paramdumpfname = '../data/paramdump.txt'
-        paramdump = device.command(ser,'paramdump')
-        with open(paramdumpfname, 'w') as paramdumpfile:
-            paramdumpfile.write(paramdump)
+    #print out unsettable params
+    unsettable_params = [param for param in bad_params if param not in device.set_commands.keys()]
+    if unsettable_params:
+        print('Unsetable invalid parameters:')
+        for param in unsettable_params:
+            print(param)
+    else:
+        print('No unsetable invalid parameters')
+        
+    paramfile.close()
+    
+def all_compasses(compass, command, *args):
+    for i in range(8):
+        input = ""
+        if(compass[i].installed):
+            device.cal(compass[i].ser,command, *args)
 
-        #load/parse params and print out
-        with open(paramdumpfname, 'r') as paramdumpfile:
-            paramdict = params.paramdump_to_dict(paramdumpfile.read())
-        print("Parameters:")
-        for key,value in paramdict.items():
-            print("{0} = {1}".format(key,value))
-        time.sleep(5.0)
+            
+def calibrate_xy(compass):                    
+    input("Level compass module and press <Enter> to begin XY calibration")
 
-        #load conditions are determine params outside of valid range
-        conditions = params.param_conditions('../data/param_conditions.csv')
-        bad_params = params.invalid_params(paramdict,conditions)
+    all_compasses(compass, "XY_Cal")
 
-        #update settable params
-        settable_params = [param for param in bad_params if param in device.set_commands.keys()]
-        if settable_params:
-            print("\nSetting parameters")
-            for param in settable_params:
-                current = paramdict[param]
-                target = params.valid_param(conditions[param])
-                device.command(ser,param,target)
-                print("{param} : {current} --> {target} Done".format(param=param,
-                                                                     current = current,
-                                                                     target = target))
-        else:
-            print("No setable invalid parameters")
+    input("Rotate Compass 360 degrees over 20 seconds then press <Enter>")
 
-        #print out unsettable params
-        unsettable_params = [param for param in bad_params if param not in device.set_commands.keys()]
-        if unsettable_params:
-            print('\nUnsetable invalid parameters:')
-            for param in unsettable_params:
-                print(param)
-        else:
-            print('\nNo unsetable invalid parameters:')
+    all_compasses(compass, "Step")
+
+def calibrate_z(compass):
+    input("Roll Compass to 90 degrees to perform Z axis calibration. Press <Enter> when ready")
+
+    all_compasses(compass, "Z_Cal")
+
+    input("Rotate Compass 360 degrees over 20 seconds then press <Enter>")
+
+    all_compasses(compass, "Step")
+
+def calibrate_softiron(compass):    
+    direction = ["North, 0", "East, 90", "South, 180", "West, 270"]   
+    all_compasses(compass, "Soft_Iron", "2")
+    for i in range(3):
+        input("Align exactly %s degrees, then hit <Enter>" % direction[i])
+        all_compasses(compass, "Soft_Step")
+    
+    input("Align exactly %s degrees, then hit <Enter>" % direction[3])
+    all_compasses(compass, "SoftIron_Step")
+        
+
