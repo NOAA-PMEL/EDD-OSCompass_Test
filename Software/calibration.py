d414765f0147735ea9602229bd02e12bd489c693
Danny Devereaux <33729388+ddevereaux@users.noreply.github.com>
2018-01-16 13:57:13 -0800

    Add files via upload

---
+++ Software/calibration.py
@@ -0,0 +1,118 @@
+import errno
+import csv
+import time
+import os
+
+import device
+import params
+
+
+def parameter_dump(compass, filename):
+    '''
+    Run automated parameter setting and calibration for the device at the specified port
+    '''
+    
+    # dump params into log
+    paramdump = device.command(compass.ser,'paramdump')
+    compass.serialNumber = (paramdump[paramdump.find("Serial_number=")+14:paramdump.find("Test_date")]).strip("\r\n")
+    
+    
+    #Create Compass Serial Number directory if non-existent. 
+    try:
+        os.mkdir('%s/%s/' % (os.getcwd(), compass.serialNumber))
+    except OSError as exc:
+        if exc.errno != errno.EEXIST:
+            raise  
+        pass
+    
+    #Save parameters into serial number directory
+    paramfname = '%s/%s/%s.txt' % (os.getcwd(), compass.serialNumber, filename)
+    with open(paramfname, 'w') as paramfile:
+        paramfile.write(paramdump)
+    paramfile.close()
+    
+    
+def parameter_check(compass, filename):    
+
+    paramfname = '%s/%s/%s.txt' % (os.getcwd(), compass.serialNumber, filename)
+    #load/parse params and print out
+    with open(paramfname, 'r') as paramfile:
+        paramdict = params.paramdump_to_dict(paramfile.read())
+    print("Saving parameters to %s.txt in directory: /%s/" % (filename, compass.serialNumber) )
+    #for key,value in paramdict.items():
+        #print("{0} = {1}".format(key,value))
+    
+    #load conditions are determine params outside of valid range
+    conditions = params.param_conditions('./data/param_conditions.csv')
+    bad_params = params.invalid_params(paramdict,conditions)
+    if(bad_params):
+        print("Bad Params: %s" % bad_params)
+    
+    #update settable params
+    settable_params = [param for param in bad_params if param in device.set_commands.keys()]
+    if settable_params:
+        print("Setting parameters:")
+        for param in settable_params:
+            current = paramdict[param]
+            target = int(params.valid_param(conditions[param]))
+            #print("Target: %d" % target)
+            device.command(compass.ser, param, target)
+            print("{param} : {current} --> {target} Done".format(param=param,
+                                                                 current = current,
+                                                                 target = target))
+    else:
+        print("No settable invalid parameters")
+
+    #print out unsettable params
+    unsettable_params = [param for param in bad_params if param not in device.set_commands.keys()]
+    if unsettable_params:
+        print('Unsetable invalid parameters:')
+        for param in unsettable_params:
+            print(param)
+    else:
+        print('No unsetable invalid parameters')
+        
+    paramfile.close()
+    
+def all_compasses(compass, command, *args):
+    for i in range(8):
+        input = ""
+        if(compass[i].installed):
+            device.cal(compass[i].ser,command, *args)
+
+            
+def calibrate_xy(compass):                    
+    input("Level compass module and press <Enter> to begin XY calibration")
+
+    all_compasses(compass, "XY_Cal")
+
+    input("Rotate Compass 360 degrees over 20 seconds then press <Enter>")
+
+    all_compasses(compass, "Step")
+
+def calibrate_z(compass):
+    input("Roll Compass to 90 degrees to perform Z axis calibration. Press <Enter> when ready")
+
+    all_compasses(compass, "Z_Cal")
+
+    input("Rotate Compass 360 degrees over 20 seconds then press <Enter>")
+
+    all_compasses(compass, "Step")
+
+def calibrate_softiron(compass):    
+    direction = ["North, 0", "East, 90", "South, 180", "West, 270"]   
+    all_compasses(compass, "Soft_Iron", "2")
+    for i in range(3):
+        input("Align exactly %s degrees, then hit <Enter>" % direction[i])
+        all_compasses(compass, "Soft_Step")
+    
+    input("Align exactly %s degrees, then hit <Enter>" % direction[3])
+    all_compasses(compass, "SoftIron_Step")
+ 
+
+def current_monitor(compass):
+	compass.current = input("How much current is Compass #%s using?" % compass.serialNumber ) #compass.port
+
+#def set_owner(compass, owner):
+
+#def set_testDate(compass):
---
+++ Software/params.py
@@ -0,0 +1,92 @@
+import csv
+from distutils.version import LooseVersion
+from datetime import datetime
+
+def paramdump_to_dict(paramdump):
+    '''
+    Returns comparable value dictionary from parameter dump string
+    '''
+    lines = [line for line in paramdump.replace("\r","").split('\n') if line not in '\n']
+    paramdict_raw = dict([line.split('=') for line in lines[1:-1]])
+    paramdict_raw['footer'] = lines[-1]
+    paramdict = {param:parse_param_value(param,value) for param,value in paramdict_raw.items()}
+    return paramdict
+
+def param_conditions(fname):
+    '''
+    Return a dict of dict containing maximum, minimum and expected paramter values for each parameter.
+    '''
+    with open(fname) as csv_file:
+        param_conditions = csv.reader(csv_file, delimiter=',')
+        next(param_conditions)
+        conditions = {param : {'min' : parse_param_value(param,minval),
+                           'max' : parse_param_value(param,maxval),
+                           'expected' : parse_param_value(param,expectedval)}
+                      for param,minval,maxval,expectedval in param_conditions}
+        #del conditions['FW_Version']
+    return conditions
+
+def is_valid_param(param,condition):
+    '''
+    Returns True for params equal to expected value or, if no vallue expected, within interval of min/max.
+    Returns False for params that don't meet these expectation or are nulllike.
+    '''
+    if param is None:
+        return False
+    else:
+        if condition['expected'] is None:
+            return condition['min'] <= param <= condition['max']
+        else:
+            return param == condition['expected']
+
+def valid_param(condition):
+    '''
+    Returns a valid target parameter based on the condition
+    (currently middle of valid interval value if expected not specified).
+    '''
+    if condition['expected'] is None:
+        return (condition['min']+condition['max'])/2
+    else:
+        return condition['expected']
+
+
+def invalid_params(paramdict,conditions):
+    '''
+    Return a list of invalid params in the paramdict based on the specified conditions
+    '''
+    return [param for param,condition in conditions.items() if not is_valid_param(paramdict[param],condition)]
+
+def parse_param_value(param,value):
+    '''
+    Parses the raw param string value based on param name.
+
+    NOTES:
+    Empty strings represented as None
+    All num-like values converted to floating points.
+    Date parsing valid through 2099 (see parse_date_str for details)
+    '''
+    if value is '':
+        return None
+    elif param == 'FW_Date':
+        normdatestr = "{0:0>2} {1} {2}".format(*value.split('-'))
+        return datetime.strptime(normdatestr, '%d %b %Y')
+    elif param == 'Test_date':
+        normdatestr = "{0:0>2} {1} 20{2}".format(*value.split(' '))
+        return datetime.strptime(normdatestr, '%d %b %Y')
+    elif param == 'FW_Version':
+        return LooseVersion(value.replace('V','').replace('-','.'))
+    elif param == 'Serial_number':
+        return float(value.replace('[D',''))
+    elif param in ['Depth_Units','footer']:
+        return value
+    elif param == 'Model':
+        return value
+    #added by alex turpin for integer case
+    elif not value.find('.'):
+        return int(value)
+    else:
+        return float(value)
+
+def parse_date_str(datestr,sep = ' '):
+    normdatestr = "{0:0>2} {1} 20{2}".format(*datestr.split(sep))
+    return datetime.strptime(normdatestr, '%d %b %Y')
