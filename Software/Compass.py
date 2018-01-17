11f0ed138e8b6513584c95bc5ce044d786c42971
AlexMTurpin <alex.turpin@noaa.gov>
2018-01-17 13:46:59 -0800

    Update Compass.py

--- Software/Compass.py
+++ Software/Compass.py
@@ -1,30 +1,35 @@
 #OS4000T Class for Compass Calibration Software
-
+import math
 import time
-import serial
 import os, stat, sys, glob, serial
 import calibration
 import device
+import csv
+import pandas
 
+MAX_COMPASS_DEVIATION=1.0
+MAX_PITCH_DEVIATION=2.0
+MAX_ROLL_DEVIATION=2.0
 
 class OS4000T():
 
-    def __init__(self, port):
-        
+    def __init__(self, port, baud):
+
         self.port = port
-        self.ser=self.Open_ComPort()
+        self.ser=self.Open_ComPort(baud)
         self.installed=False
         #Grab example output from compass. Ex: $C106.3P-15.6R46.0T20.4*0A
         self.grab_compass_example()
         self.serialNumber = 0
         self.current = 0
-		
-	
+        self.testResult = False
+        self.deviation = 0.0
         
-    def Open_ComPort(self):
+        
+    def Open_ComPort(self, baud):
         
         conn = serial.Serial(port=self.port,
-            baudrate=19200,
+            baudrate=baud,
             parity=serial.PARITY_NONE,
             stopbits=serial.STOPBITS_ONE,
             bytesize=serial.EIGHTBITS,
@@ -38,8 +43,12 @@ class OS4000T():
         input = self.ser.readline().decode('utf-8', 'ignore')
         if(input.find("$")!=-1):
             if(input.rfind("*")!=-1):
-                print("%s: %s" % (self.port, input[input.rfind("$"):input.rfind("*")+3]))
+                ex = input[input.rfind("$"):input.rfind("*")+3]
+                print("%s: %s" % (self.port, ex))
                 self.installed=True
+                return ex
+                
+
                 
    
 def Setup_ComPorts():
@@ -69,47 +78,150 @@ def Setup_ComPorts():
     print(portnames)
     return(portnames)
     
-""" Moved to Calibration.py    
-def all_compasses(compass, command, *args):
-    for i in range(8):
-        input = ""
-        if(compass[i].installed):
-            device.cal(compass[i].ser,command, *args)
-""" 
+
+def write_csv(compass, date, owner):
+    
+    if(os.path.isfile('OS4000T.csv')==False):
+        f = open('OS4000T.csv', 'a')
+        f.write("Serial Number,Deviation,PMEL Owner,Test Date,Compass Current,Test Result\r\n")
+    else:
+        f = open('OS4000T.csv', 'a')
+    f.write("%s,%s,%s,%s,%s,%s\r\n" %(compass.serialNumber, compass.deviation, owner, date, compass.current, compass.testResult))
+    f.close()
+    
+def update_csv(compass, timenow):
+    with open('OS4000T.csv', newline='') as csvfile:
+        CompassReader = pandas.read_csv(csvfile, delimiter=',')
+        df = pandas.DataFrame(data=CompassReader)
         
+        
+        df = df.set_index('Serial Number')
+        df = df.sort_index()
+        df = df[~df.index.duplicated(keep='last')]
+        
+            
+        for i in range(8):
+            if(compass[i].installed):
+                df.ix[compass[i].serialNumber, 0] = compass[i].deviation
+                df.ix[compass[i].serialNumber, 2] = timenow
+                df.ix[compass[i].serialNumber, 4] = compass[i].testResult
+            
+    print(df)
     
-Ports=Setup_ComPorts()
-compass=[]
-
-date= input('Test Date mmddyyyy: ')
-owner= input('PMEL Owner: ')
-
-for i in range(8):
-    compass.append(OS4000T(Ports[len(Ports)-8+i]))
+    #Write sorted file to list.
+    dataList.to_csv('OS4000T_Master.csv')
     
-for i in range(8):
-    if(compass[i].installed):
-        calibration.parameter_dump(compass[i], "Params_PreCal")
-        calibration.parameter_check(compass[i], "Params_PreCal")
+    
+def compass_setup(Ports):
+    compass=[]
 
-for i in range(8):
-	if(compass[i].installed):
-		calibration.current_monitor(compass[i])		
-		
-for i in range(8):
-	if(compass[i].installed):
-		print("Compass %s: SN# %s" % (i, compass[i].serialNumber))
+    
+    owner= input('PMEL Owner: ')
 
-calibration.calibrate_xy(compass)
+    for i in range(8):
+        compass.append(OS4000T(Ports[len(Ports)-8+i], 9600))
+        
+    for i in range(8):
+        if(compass[i].installed):
+            calibration.parameter_dump(compass[i], "Params_PreCal")
+            calibration.parameter_check(compass[i], "Params_PreCal")
+    
+    print("Compass current testing. 1 milli-Volt equals 10 milli-Amp")     
+    for i in range(8):
+        if(compass[i].installed):
+            calibration.current_monitor(compass[i])		
+    
+    datetime = time.strftime("%m%d%Y_%H:%M:%S", time.gmtime())        
+    for i in range(8):
+        if(compass[i].installed):
+            write_csv(compass[i], datetime, owner)
+            
+    input("Unplug USB device, reconnect, then wait 10 seconds for settings to take effect. Then press enter.")            
 
-calibration.calibrate_z(compass)
 
-calibration.calibrate_softiron(compass)
+def compass_calibrate(Ports):
+    compass=[]
+    
+    for i in range(8):
+        compass.append(OS4000T(Ports[len(Ports)-8+i], 9600))
+            
+    calibration.calibrate_xy(compass)
 
-for i in range(8):
-    if(compass[i].installed):
-        calibration.parameter_dump(compass[i], "Params_PostCal")
-		
+    calibration.calibrate_z(compass)
 
-	
+    calibration.calibrate_softiron(compass)
+    
 
+    for i in range(8):
+        if(compass[i].installed):
+            calibration.parameter_dump(compass[i], "Params_PostCal")
+    
+    datetime = time.strftime("%m%d%Y_%H:%M:%S", time.gmtime())
+    
+    test_verification(compass)
+    
+    #Push new Compass Values
+    update_csv(compass, datetime)
+
+def test_verification(compass):
+    #Assuming they pass unless proven not below
+    for i in range(8):
+        compass[i].testResult=True
+    
+    print("Testing accuracy of calibration. ")
+    for i in range(5):
+        direction = int(input("Turn compass calibration jig to desired direction and enter degree (0-359):"))
+        if(0<=direction<=359):
+            for i in range(8):
+                if(compass[i].installed & compass[i].testResult):
+                    ex = compass[i].grab_compass_example()
+                    c = ex[ex.find("$C")+2:ex.rfind("P")]
+                    result = math.fabs(float(c)-direction)
+                    print("Compass %s deviation: %s" %(compass[i].serialNumber, result))
+                    if(result>=MAX_COMPASS_DEVIATION):
+                        compass[i].testResult=False
+                        print("Compass Failed!")
+
+    print("Testing Pitch Accuracy")
+    for i in range(4):
+        pitch = int(input("Pitch compass calibration jig to desired angle (-75 to 75):"))
+        if(-75<=pitch<=75):
+            for i in range(8):
+                if(compass[i].installed & compass[i].testResult):
+                    ex = compass[i].grab_compass_example()
+                    p = ex[ex.find("P")+1:ex.rfind("R")]
+                    result = math.fabs(float(p)-pitch)
+                    print("Compass %s pitch deviation: %s" %(compass[i].serialNumber, result))
+                    if(result>=MAX_PITCH_DEVIATION):
+                        compass[i].testResult=False
+                        print("Compass Failed!")
+    
+    print("Testing Roll Accuracy")
+    for i in range(4):
+        roll = int(input("Roll compass calibration jig to desired angle (-75 to 75):"))
+        if(-75<=roll<=75):
+            for i in range(8):
+                if(compass[i].installed & compass[i].testResult):
+                    ex = compass[i].grab_compass_example()
+                    r = ex[ex.find("R")+1:ex.rfind("T")]
+                    result = math.fabs(float(r)-roll)
+                    print("Compass %s roll deviation: %s" % (compass[i].serialNumber, result))
+                    if(result>=MAX_ROLL_DEVIATION):
+                        compass[i].testResult=False
+                        print("Compass Failed!")
+    
+    
+
+Ports=Setup_ComPorts()	
+
+while(1):
+
+    char = input("Compass Setup (\'s\') or Compass Calibrate (\'c'\')?")
+    print("Input Char: %s" % char)
+    
+    if(char=='s'):
+        compass_setup(Ports)
+    elif(char=='c'):
+        compass_calibrate(Ports)
+    elif(char==''):
+        break
